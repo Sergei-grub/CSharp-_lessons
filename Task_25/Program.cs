95f5240cc2d8cfca404095ff507f2b9281854fb4
Sergei <sergeygrubber@gmail.com>
2023-03-09 01:05:50 +0300

    Решение для задачи N 25

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 
+*/
+void FindRate (int userNum, int userRate)
+{
+    int rate = 1;
+    for (int i = 0; i < userRate; i++)
+    {
+        rate = rate * userNum;    
+    }
+  Console.WriteLine($"Результат возведения в степень: {rate}");
+}
+Console.Write("Введите число для возведения в степень: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень для заданного числа: ");
+int userRate = Convert.ToInt32(Console.ReadLine());
+
+FindRate (userNum, userRate);
+
