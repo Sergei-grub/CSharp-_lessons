628f5a878bcccf7217d497e8d07aff93241f587d
Sergei <sergeygrubber@gmail.com>
2023-03-09 00:19:37 +0300

    Решение для задачи N 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 
+*/
+void FindCube (int userNum)
+{
+  for (int i = 1; i <= userNum; i++)
+  {
+    int result = i * i * i;
+    string comma = ",";
+    if (i == userNum) Console.Write($"{result} ");
+    else Console.Write($"{result}{comma} ");
+  }
+}
+Console.Write("Введите число для возведения в 3 степень: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+FindCube (userNum);
