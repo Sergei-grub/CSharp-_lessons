18c035c6da26d8848d7c0ff83617ff314d0c3488
Sergei <sergeygrubber@gmail.com>
2023-03-19 19:55:52 +0300

    Решение для задачи N 47

---
+++ Task_47/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+ */
+
+double[,] PrArray(int row, int column)
+{
+    double min = -10;
+    double max = 10;
+    double[,] arr = new double [row, column];
+    Random rnd = new Random();
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = rnd.NextDouble() * (max - min) + min;
+            Console.Write($"{arr[i,j]:0.#}\t");
+        }
+        Console.WriteLine();    
+    }
+    return arr;
+}
+
+Console.Write("Введите количество строк массива: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int column = Convert.ToInt32(Console.ReadLine());
+double[,] array = PrArray(row, column);
---
+++ Task_50/Program.cs
@@ -0,0 +1,44 @@
+﻿/* 
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+ */
+
+void FindInRndArray(int userNum)
+{
+    bool result = false;
+    int count = 0;
+    int[,] arr = new int [3,4];
+    Random rnd = new Random();
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = rnd.Next(1, 10);
+            if(userNum == arr[i,j]) 
+            {
+                result = true;
+                count++;
+            }
+            Console.Write($"{arr[i,j]:0.#}\t");            
+        }
+        Console.WriteLine();           
+    }
+    if(result == true && count > 1 && count < 5 && count > 1)  //проверка для окончания слова "раз" исходя из количтества повторов
+    {
+        Console.WriteLine($"В массиве есть заданное значение — {userNum} и оно встречается {count} раза.");
+    }
+    else if (result == true && count > 4) //проверка для окончания слова "раза" исходя из количтества повторов
+    {
+        Console.WriteLine($"В массиве есть заданное значение — {userNum} и оно встречается {count} раз.");
+    }
+    else Console.Write($"{userNum} -> такого числа нет в массиве."); 
+}
+
+Console.Write("Введите искомое в массиве число: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+FindInRndArray(userNum);
