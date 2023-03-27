0021255f531eca537226c8c23cbdcd2b951fc798
Sergey <sergeygrubber@gmail.com>
2023-03-27 17:37:30 +0300

    Решение для задачи N 60

--- Task_60/Program.cs
+++ Task_60/Program.cs
@@ -6,5 +6,48 @@
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
  */
- 
+
+
+int[,,] Fill3dArray(int row, int col, int elements)
+{
+    int[,,] arr = new int[row, col, elements];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[i, j, k] = rnd.Next(0, 10);
+            }
+        }
+    }
+    return arr;
+}
+void Print(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"{arr[i, j, k]}({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+Console.Write("Введите число строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число элементов: ");
+int elements = Convert.ToInt32(Console.ReadLine());
+int[,,] arr3d = Fill3dArray(row, column, elements);
+Print(arr3d);
+Console.WriteLine();
+Print(arr3d);
 
